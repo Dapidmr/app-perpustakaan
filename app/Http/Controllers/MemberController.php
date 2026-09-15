22a889d29065a588064cca4f282b8b0386a53f9b
Dapidmr <davidmr1905@gmail.com>
2026-09-15 11:33:35 +0700

    [PK-3] tambah controller member dan validasi member

--- app/Http/Controllers/MemberController.php
+++ app/Http/Controllers/MemberController.php
@@ -2,16 +2,49 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreMemberRequest;
 use Illuminate\Http\Request;
 
 class MemberController extends Controller
 {
+    private array $members = [
+        [
+            'id' => 1,
+            'nama' => 'Andi Pratama',
+            'nim' => '2301001',
+            'email' => 'andi@gmail.com',
+            'nomor_telepon' => '081234567890',
+            'alamat' => 'Surabaya',
+            'status' => 'Aktif',
+        ],
+        [
+            'id' => 2,
+            'nama' => 'Budi Santoso',
+            'nim' => '2301002',
+            'email' => 'budi@gmail.com',
+            'nomor_telepon' => '082345678901',
+            'alamat' => 'Sidoarjo',
+            'status' => 'Aktif',
+        ],
+        [
+            'id' => 3,
+            'nama' => 'Citra Lestari',
+            'nim' => '2301003',
+            'email' => 'citra@gmail.com',
+            'nomor_telepon' => '083456789012',
+            'alamat' => 'Gresik',
+            'status' => 'Tidak Aktif',
+        ]
+    ];
+
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        return 'MemberController@index';
+        $members = $this->members;
+
+        return view('members.index', compact('members'));
     }
 
     /**
@@ -19,15 +52,21 @@ class MemberController extends Controller
      */
     public function create()
     {
-        return 'MemberController@create';
+        return view('members.create');
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreMemberRequest $request)
     {
-        return 'MemberController@store';
+        $validated = $request->validated();
+
+        return redirect()->route('members.index')
+            ->with(
+                'success',
+                "Member \"{$validated['nama']}\" berhasil ditambahkan (data dummy, belum tersimpan ke database)."
+            );
     }
 
     /**
